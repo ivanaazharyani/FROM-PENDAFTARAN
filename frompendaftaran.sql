f2ef8e2cd3c8fcd58c8476cc1a98420fcb49f9f4
ivanaazharyani <ivanaazharyani@gmail.com>
2020-01-08 13:29:54 +0700

    Add files via upload

---
+++ frompendaftaran.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 08, 2020 at 07:01 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `frompendaftaran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `frompendaftran`
+--
+
+CREATE TABLE `frompendaftran` (
+  `nama` text,
+  `tanggal` date NOT NULL,
+  `jenis_kelamin` point NOT NULL,
+  `alamat` text NOT NULL,
+  `daftarriwayatpekerjaan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
